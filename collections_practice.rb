b2c7306392e3b1ce21a2b3cd42a5d12337eca5bd
Christine Myers Keitt <cmyerskeitt@gmail.com>
2020-07-15 22:04:58 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,35 @@
+def sort_array_asc(array)
+  return array.sort 
+end
 
+def sort_array_desc(array)
+  array.sort do |a, b|
+  b <=> a
+  end
+  #array.sort_by { |number| -number }
+end 
+
+def sort_array_char_count(array) 
+ array.sort_by(&:length)
+end 
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  return array.reverse
+end
+
+def kesha_maker(array)
+  array.each {|i| 
+    i[2] = "$"}
+end
+
+def find_a(array)
+  return array.select {|word| word.start_with?("a")}  
+end
+
+
+  
